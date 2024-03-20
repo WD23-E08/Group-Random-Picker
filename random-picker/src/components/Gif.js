3005f63fe31d9f425186b29578d333c64c78e7ce
Mir Ahmad Niazi <mir.niazi@dci-student.org>
2024-03-20 16:12:48 +0100

    step 3 updated

--- random-picker/src/components/Gif.js
+++ random-picker/src/components/Gif.js
@@ -2,7 +2,9 @@ export const gif = [
     "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3k3NDAyODMyZTFscTBscm13anBub2ZlNnFvMnZzNWZ2Nm8wY3l1aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vVzH2XY3Y0Ar6/giphy.gif",
     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3d0bmNkMmYxMTR3NHQzY290bjNocGUyeWpsbmZpbXBubDg0dzRjYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/uU3oBlPJTx184/giphy.gif",
     "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZWsybmRwdTk0NXplcjF6bGY4eG80ajMwY2Fldmd6dXBwNnNjajE3NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/10eXQuHPIbWK2I/giphy.gif",
-    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnV1ajY1OTY5eGNwcWcwZm9iNzRkbXU3YWlkZW5sZGhnMmtrNHkyayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6Ze96VQjmecIo/giphy.gif"
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnV1ajY1OTY5eGNwcWcwZm9iNzRkbXU3YWlkZW5sZGhnMmtrNHkyayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6Ze96VQjmecIo/giphy.gif",
+    "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExN2E2NmU3aHNiZ292Ym5vaW02a2U1Zm0wa3M5NmkzNnZ3Y21jbmphaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vFKqnCdLPNOKc/giphy.gif",
+    "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExajZ0d3JhYWxrcmV4OTRvdjc3dTk1ZWQ4NmNpeXF6bDAxMG96dGJ3YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/efHwZH4DeN9ss/giphy.gif"
     ]
     
     
